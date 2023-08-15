d27f9eee56406b079ac31ab94a862d313dde82cc
Abdelfettah Amil <abdel.fattah.amil@ibm.com>
2023-08-15 17:15:38 +0100

    first clone for backend

---
+++ Back-end-Project/src/main/java/fattahAmil/BackendProject/BackEndProjectApplication.java
@@ -0,0 +1,13 @@
+package fattahAmil.BackendProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndProjectApplication.class, args);
+	}
+
+}
